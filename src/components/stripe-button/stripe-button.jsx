b2df060a14b9fe3314b1ee2a54406adf7757f3c0
Kaian Davila Lima <kaiandlima@gmail.com>
2021-01-11 18:00:37 -0400

    stripe integration, favicon and title

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51I8HZpEI6Ytr953eO6GpBh39tfn5EclFr5zG91fLmlycprlri5GHW2wJoLGz2DJVK1SBShqOR9tfbghw0EjgO0ev00X57C9HNc";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
